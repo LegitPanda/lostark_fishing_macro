1a49e462d57f50975aa86f0b87d4cd46f3b590fd
Eric Xing <hcxing@uwaterloo.ca>
2022-04-17 18:18:49 -0400

    Optimized detection (10x less cpu usage), update readme
    added stop after too many failures
    clean up info message

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ numpy
 
 ## Instruction
 1. Install libraries mentioned in "Prerequisites" section.  
-2. Place "fishcaught.png" in your desktop.  
+2. Place "fishcaught.png" in the same directory as the script.  
 3. Run "fish_macro.py" script.  
 4. Place your character in Lost Ark game near fishing location. Cast a fishing rod by clicking w.  
 5. You are all set!  
@@ -20,8 +20,8 @@ This script knows to pull the fishing rod out when the fish is caught by detecti
 It waits until your character is ready, and casts another fishing rod.
 
 ## Troubleshooting
-Denpending on your screen/game's resolution, image provided might not match actuaal exclamation mark for you.
-Simply take a screenshot and replacee provided image with your own.
+Depending on your screen/game's resolution, image provided might not match actual exclamation mark for you.
+Simply take a screenshot and replace provided image with your own.
 
 ## Contributions
 Thanks to drov0's python-imagesearch : https://github.com/drov0/python-imagesearch
--- fish_macro.py
+++ fish_macro.py
@@ -1,6 +1,3 @@
-import os
-# Automatically set the user's desktop as file path
-os.chdir(os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop'))
 import win32com.client
 import pyautogui
 import cv2
@@ -12,56 +9,79 @@ import numpy as np
 shell = win32com.client.Dispatch("WScript.Shell")
 
 # imagesearch source = github : https://github.com/drov0/python-imagesearch
-def imagesearch(image, precision=0.8):
-    im = pyautogui.screenshot()
-    #im.save('testarea.png') usefull for debugging purposes, this will save the captured region as "testarea.png"
+
+# replace this with your own fish caught image if detection is off
+image = 'fishcaught.png'
+template = cv2.imread(image, 0)
+template.shape[::-1]
+
+start_x, start_y = pyautogui.size()
+
+
+def imagesearch(precision=0.7):
+    # tested on 1920 x 1080, increase/adjust offsets if failing
+    im = pyautogui.screenshot(region=(start_x/2-50, start_y/2-100, 100, 100))
+    # im.save(r'R:\testarea{}.png'.format(time.time())) #usefull for debugging purposes, this will save the captured region as "testarea.png"
     img_rgb = np.array(im)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    template = cv2.imread(image, 0)
-    template.shape[::-1]
 
     res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     if max_val < precision:
-        return [-1,-1]
+        # print("not found", max_val)
+        return None
+    # print(min_val, max_val, min_loc, max_loc)
     return max_loc
+
 
 # 1 fish per about 23 sec. fishing rod 100 = 37 min. If you miss fish, it takes 33 sec to cast again.
 # https://medium.com/@martin.lees/image-recognition-for-automation-with-python-711ac617b4e5
 print("[FYI #1] 100 fishing rods = about 37 minutes OR 157 fishing rods = about 1 hour")
 print(" ")
-number_of_fishing_rods = 999
-print(" ")
 print("[FYI #2] ctrl + c <- exit the program")
 print(" ")
 print("Running...")
-print("Made by silvernine")
+print("Made by silvernine, LegitPandas")
 
-# counts # of caught fish
-caught_fish_counter = 0
+caught_fish = 0
+total_tries = 0
+consecutive_failed_tries = 0
 timeout_start_fishing = time.time()
-for i in range(int(number_of_fishing_rods)):
-    # 34 seconds timeout for failed detection case.
-    timeout = 34 
-    #starting time
-    timeout_start = time.time()
-    # Initial value to keep the while loop started
-    pos_fish=[-1,-1]
-    # while fish is not caught yet or under specified timeout, keep looking for the ! sign
-    while (pos_fish[0]==-1) and (time.time()<(timeout_start+timeout)):
-        pos_fish=imagesearch("fishcaught.png")
-    # Send 'w' keystroke when fish is caught OR recast the bait if detection failed and timed out
-    shell.SendKeys("w")
-    # Recast the bait if detection was successful. Won't send 'w' if detection failed.
-    if (time.time()-timeout_start)<timeout and i<(int(number_of_fishing_rods)-1):
-        time.sleep(6) #time it takes to re-cast after pulling the bait out
-        time.sleep(random.randint(15,20)/10) #add 1~2 sec random time
-        shell.SendKeys("w")
-        caught_fish_counter += 1
-print("Done!")
-print(" ")
-print("You caught {} fish out of {} tries. Success rate is {}%.".format(int(caught_fish_counter),int(number_of_fishing_rods),round(int(caught_fish_counter)/int(number_of_fishing_rods)*100)))
-print("It took {} minutes. Program will exit automatically in 30 seconds.".format(round((time.time()-timeout_start_fishing)/60)))
-time.sleep(30)
-        
 
+# 34 seconds timeout for failed detection case.
+timeout = 34
+while(True):
+    # starting time
+    timeout_start = time.time()
+
+    # Initial value to keep the while loop started
+    pos_fish = None
+
+    # while fish is not caught yet or under specified timeout, keep looking for the ! sign
+    while (pos_fish == None) and (time.time() < (timeout_start+timeout)):
+        pos_fish = imagesearch()
+        time.sleep(.2)
+
+    if (pos_fish == None):
+        consecutive_failed_tries += 1
+    else:
+        consecutive_failed_tries = 0
+
+    if (consecutive_failed_tries > 2):
+        print('Stopped due to too many failures')
+        break
+
+    # Send 'e' keystroke when fish is caught OR recast the bait if detection failed and timed out
+    shell.SendKeys("e")
+
+    # Recast the bait if detection was successful. Won't send 'e' if detection failed.
+    if (time.time()-timeout_start) < timeout:
+        time.sleep(5)  # time it takes to re-cast after pulling the bait out
+        time.sleep(random.randint(15, 20)/10)  # add 1~2 sec random time
+        shell.SendKeys("e")
+        time.sleep(7)
+        caught_fish += 1
+
+    total_tries += 1
+    print("You caught {} fish out of {} tries. Success rate is {}%.".format(int(caught_fish), int(
+        total_tries), round(int(caught_fish)/int(total_tries)*100)))
